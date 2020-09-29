f5943db9de655f6f623735dfe852633577f37c47
Anfii <Anthonywilde117@gmail.com>
2020-09-29 17:08:47 +1000

    played with mongodb

--- test/index.test.js
+++ test/index.test.js
@@ -8,3 +8,22 @@ test("test2 blank space", () => {
     expect(() => {
         greetEachPerson();}).toThrow("Please provide valid names to greet")
 });
+
+test("string amd non string", () => {
+    expect(() => {
+        greetEachPerson("string", 1, "fart", 69, ["cheese"], {hash: "weeds"}).toEqual(["Hello there, string!", "Hello there, fart!"])
+    })})
+
+test("test4 blank space", () => {
+        expect(() => {
+            greetEachPerson("        ");}).toThrow("Please provide valid names to greet")
+    });
+
+ test("samemyname", () => {
+        expect(greetEachPerson("69696969","mynamejeff")).toEqual(["Hello there, mynamejeff!"])
+        });
+
+test("TeStDrIvEnDeVelOpMeNt", () => {
+    expect(() => {
+        (greetEachPerson("69696969", "________" , "#trendu"));}).toThrow("Please provide valid names to greet")
+});
